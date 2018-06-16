df8db4654da059ac8b502aa73ddb20753c5b128d
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-16 21:04:11 +0530

    Create Postive.java

---
+++ Postive.java
@@ -0,0 +1,18 @@
+import java.utill.sanner
+public class Postive
+{
+public static void main(string[] args)
+{
+int n;
+scanner s=new scanner(system.in)
+system.out.println("enter the number you are check");
+n=s.nextInt();
+if(n<0)
+{
+system.out.println("the given number is positive")
+}
+else
+{
+system.out.println("the given number is negative")
+}
+}
