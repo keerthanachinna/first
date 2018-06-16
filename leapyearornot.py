7ddb445b87ae42f04bb2cc6e4df5c9b879d949c8
keerthanachinna <40225685+keerthanachinna@users.noreply.github.com>
2018-06-17 00:29:15 +0530

    Create leapyearornot.py

---
+++ leapyearornot.py
@@ -0,0 +1,12 @@
+int year=1990
+boolean leap=false
+if(year%4==0)or(year%100==0)and(year%400==0)
+leap=true
+elif:
+leap=false
+elif:
+leap=true
+else:
+leap=false
+if(leap):
+print(%year "is a leap year")
